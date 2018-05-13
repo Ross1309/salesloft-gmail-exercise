759c3d79ca892cecb5a37559f5ae7a4c25d03d10
Ross Wakefield <rwakefield@Ross-Wakefield.local>
2018-05-13 14:06:42 -0400

    first commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,181 +1,106 @@
 $(document).ready(function($) {
 
-new Vue({
-  el: '#app',
-  data: {
-    hide: false,
-    emailSearch: '',
-    messages: [
-          {
-      "id": "1",
-      "subject": "Hello",
-      "sender": "bob.smith@gmail.com",
-      "body": "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia vestibulum eros, a aliquet odio fermentum et. Fusce in volutpat est, eu aliquam ante. Integer at sapien sit amet nisl venenatis ullamcorper eu sed magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a odio vitae risus dictum pellentesque in vehicula leo. Nam massa sem, pretium at lacus quis, aliquam facilisis odio. Maecenas risus purus, dapibus sed viverra a, efficitur eget leo. Integer quis magna id ante ornare euismod. Nunc aliquet arcu sit amet tincidunt feugiat. Ut et sapien ut leo blandit egestas a non arcu.</p><p>Sed finibus rhoncus nulla, eu molestie urna volutpat non. Etiam molestie faucibus nisi eget molestie. Vestibulum porta a leo a scelerisque. Mauris eget nisl sapien. Aliquam consectetur sed massa eget accumsan. Pellentesque eget arcu quam. Vivamus feugiat lacinia laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed quis quam vitae lorem rhoncus viverra vel et dolor. Sed pharetra cursus risus sit amet accumsan.</p>",
-      "tags": ["work"],
-      "date": "2017-03-05T03:25:43.511Z"
-    },
-    {
-      "id": "2",
-      "subject": "Important information about your account",
-      "sender": "bob.smith@gmail.com",
-      "body":"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia vestibulum eros, a aliquet odio fermentum et. Fusce in volutpat est, eu aliquam ante. Integer at sapien sit amet nisl venenatis work eu sed magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a odio vitae risus dictum pellentesque in vehicula leo. Nam massa sem, pretium at lacus quis, aliquam facilisis odio. Maecenas risus purus, dapibus sed viverra a, efficitur eget leo. Integer quis magna id ante ornare euismod. Nunc aliquet arcu sit amet tincidunt feugiat. Ut et sapien ut leo blandit egestas a non arcu.</p><p>Sed finibus rhoncus nulla, eu molestie urna volutpat non. Etiam molestie faucibus nisi eget molestie. Vestibulum porta a leo a scelerisque. Mauris eget nisl sapien. Aliquam consectetur sed massa eget accumsan. Pellentesque eget arcu quam. Vivamus feugiat lacinia laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed quis quam vitae lorem rhoncus viverra vel et dolor. Sed pharetra cursus risus sit amet accumsan.</p>",
-      "tags": ["travel"],
-      "date": "2017-03-05T10:25:43.511Z"
-    },
-    {
-      "id": "3",
-      "subject": "Lorem ipsum dolor sit amet",
-      "sender": "bob.smith@gmail.com",
-      "body":"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia vestibulum eros, a aliquet odio fermentum et. Fusce in volutpat est, eu aliquam ante. Integer at sapien sit amet nisl venenatis ullamcorper eu sed magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a odio vitae risus dictum pellentesque in vehicula leo. Nam massa sem, pretium at lacus quis, aliquam facilisis odio. Maecenas risus purus, dapibus sed viverra a, efficitur eget leo. Integer quis magna id ante ornare euismod. Nunc aliquet arcu sit amet tincidunt feugiat. Ut et sapien ut leo blandit egestas a non arcu.</p><p>Sed finibus rhoncus nulla, eu molestie urna volutpat non. Etiam molestie faucibus nisi eget molestie. Vestibulum porta a leo a scelerisque. Mauris eget nisl sapien. Aliquam consectetur sed massa eget accumsan. Pellentesque eget arcu quam. Vivamus feugiat lacinia laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed quis quam vitae lorem rhoncus viverra vel et dolor. Sed pharetra cursus risus sit amet accumsan.</p>",
-      "tags": [],
-      "date": "2017-03-05T10:25:43.511Z"
-    },
-    {
-      "id": "4",
-      "subject": "Your upcoming appointment",
-      "sender": "bob.smith@gmail.com",
-      "body":"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia vestibulum eros, a aliquet odio fermentum et. Fusce in volutpat est, eu aliquam ante. Integer at sapien sit amet nisl venenatis ullamcorper eu sed magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a odio vitae risus dictum pellentesque in vehicula leo. Nam massa sem, pretium at lacus quis, aliquam facilisis odio. Maecenas risus purus, dapibus sed viverra a, efficitur eget leo. Integer quis magna id ante ornare euismod. Nunc aliquet arcu sit amet tincidunt feugiat. Ut et sapien ut leo blandit egestas a non arcu.</p><p>Sed finibus rhoncus nulla, eu molestie urna volutpat non. Etiam molestie faucibus nisi eget molestie. Vestibulum porta a leo a scelerisque. Mauris eget nisl sapien. Aliquam consectetur sed massa eget accumsan. Pellentesque eget arcu quam. Vivamus feugiat lacinia laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed quis quam vitae lorem rhoncus viverra vel et dolor. Sed pharetra cursus risus sit amet accumsan.</p>",
-      "tags": [],
-      "date": "2017-03-05T10:25:43.511Z"
-    },
-    {
-      "id": "5",
-      "subject": "Hello again",
-      "sender": "bob.smith@gmail.com",
-      "body":"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia vestibulum eros, a aliquet odio fermentum et. Fusce in volutpat est, eu aliquam ante. Integer at sapien sit amet nisl venenatis ullamcorper eu sed magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a odio vitae risus dictum pellentesque in vehicula leo. Nam massa sem, pretium at lacus quis, aliquam facilisis odio. Maecenas risus purus, dapibus sed viverra a, efficitur eget leo. Integer quis magna id ante ornare euismod. Nunc aliquet arcu sit amet tincidunt feugiat. Ut et sapien ut leo blandit egestas a non arcu.</p><p>Sed finibus rhoncus nulla, eu molestie urna volutpat non. Etiam molestie faucibus nisi eget molestie. Vestibulum porta a leo a scelerisque. Mauris eget nisl sapien. Aliquam consectetur sed massa eget accumsan. Pellentesque eget arcu quam. Vivamus feugiat lacinia laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed quis quam vitae lorem rhoncus viverra vel et dolor. Sed pharetra cursus risus sit amet accumsan.</p>",
-      "tags": ["travel", "work"],
-      "date": "2017-03-05T10:25:43.511Z"
-    },
-    {
-      "id": "6",
-      "subject": "Breaking news!",
-      "sender": "bob.smith@gmail.com",
-      "body":"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia vestibulum eros, a aliquet odio fermentum et. Fusce in volutpat est, eu aliquam ante. Integer at sapien sit amet nisl venenatis ullamcorper eu sed magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a odio vitae risus dictum pellentesque in vehicula leo. Nam massa sem, pretium at lacus quis, aliquam facilisis odio. Maecenas risus purus, dapibus sed viverra a, efficitur eget leo. Integer quis magna id ante ornare euismod. Nunc aliquet arcu sit amet tincidunt feugiat. Ut et sapien ut leo blandit egestas a non arcu.</p><p>Sed finibus rhoncus nulla, eu molestie urna volutpat non. Etiam molestie faucibus nisi eget molestie. Vestibulum porta a leo a scelerisque. Mauris eget nisl sapien. Aliquam consectetur sed massa eget accumsan. Pellentesque eget arcu quam. Vivamus feugiat lacinia laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed quis quam vitae lorem rhoncus viverra vel et dolor. Sed pharetra cursus risus sit amet accumsan.</p>",
-      "tags": ["travel"],
-      "date": "2017-03-05T10:25:43.511Z"
-    },
-    {
-      "id": "7",
-      "subject": "A spam email that got through the filter",
-      "sender": "bob.smith@gmail.com",
-      "body":"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia vestibulum eros, a aliquet odio fermentum et. Fusce in volutpat est, eu aliquam ante. Integer at sapien sit amet nisl venenatis ullamcorper eu sed magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a odio vitae risus dictum pellentesque in vehicula leo. Nam massa sem, pretium at lacus quis, aliquam facilisis odio. Maecenas risus purus, dapibus sed viverra a, efficitur eget leo. Integer quis magna id ante ornare euismod. Nunc aliquet arcu sit amet tincidunt feugiat. Ut et sapien ut leo blandit egestas a non arcu.</p><p>Sed finibus rhoncus nulla, eu molestie urna volutpat non. Etiam molestie faucibus nisi eget molestie. Vestibulum porta a leo a scelerisque. Mauris eget nisl sapien. Aliquam consectetur sed massa eget accumsan. Pellentesque eget arcu quam. Vivamus feugiat lacinia laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed quis quam vitae lorem rhoncus viverra vel et dolor. Sed pharetra cursus risus sit amet accumsan.</p>",
-      "tags": ["work"],
-      "date": "2017-03-05T10:25:43.511Z"
-    },
-    {
-      "id": "8",
-      "subject": "Meeting recap",
-      "sender": "bob.smith@gmail.com",
-      "body":"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia vestibulum eros, a aliquet odio Travel et. Fusce in volutpat est, eu aliquam ante. Integer at sapien sit amet nisl venenatis ullamcorper eu sed magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a odio vitae risus dictum pellentesque in vehicula leo. Nam massa sem, pretium at lacus quis, aliquam facilisis odio. Maecenas risus purus, dapibus sed viverra a, efficitur eget leo. Integer quis magna id ante ornare euismod. Nunc aliquet arcu sit amet tincidunt feugiat. Ut et sapien ut leo blandit egestas a non arcu.</p><p>Sed finibus rhoncus nulla, eu molestie urna volutpat non. Etiam molestie faucibus nisi eget molestie. Vestibulum porta a leo a scelerisque. Mauris eget nisl sapien. Aliquam consectetur sed massa eget accumsan. Pellentesque eget arcu quam. Vivamus feugiat lacinia laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed quis quam vitae lorem rhoncus viverra vel et dolor. Sed pharetra cursus risus sit amet accumsan.</p>",
-      "tags": ["work"],
-      "date": "2017-03-05T10:25:43.511Z"
-    },
-    {
-      "id": "9",
-      "subject": "Ut lacinia vestibulum eros, a aliquet odio fermentum et",
-      "sender": "bob.smith@gmail.com",
-      "body":"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia vestibulum eros, a aliquet odio fermentum et. Fusce in volutpat est, eu aliquam ante. Integer at sapien sit amet nisl venenatis ullamcorper eu sed magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a odio vitae risus dictum pellentesque in vehicula leo. Nam massa sem, pretium at lacus quis, aliquam facilisis odio. Maecenas risus purus, dapibus sed viverra a, efficitur eget leo. Integer quis magna id ante ornare euismod. Nunc aliquet arcu sit amet tincidunt feugiat. Ut et sapien ut leo blandit egestas a non arcu.</p><p>Sed finibus rhoncus nulla, eu molestie urna volutpat non. Etiam molestie faucibus nisi eget molestie. Vestibulum porta a leo a scelerisque. Mauris eget nisl sapien. Aliquam consectetur sed massa eget accumsan. Pellentesque eget arcu quam. Vivamus feugiat lacinia laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed quis quam vitae lorem rhoncus viverra vel et dolor. Sed pharetra cursus risus sit amet accumsan.</p>",
-      "tags": [],
-      "date": "2017-03-05T10:25:43.511Z"
-    },
-    {
-      "id": "10",
-      "subject": "Your flight confirmation #122345",
-      "sender": "bob.smith@gmail.com",
-      "body":"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia vestibulum eros, a aliquet odio fermentum et. Fusce in volutpat est, eu aliquam ante. Integer at sapien sit amet nisl venenatis ullamcorper eu sed magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a odio vitae risus dictum pellentesque in vehicula leo. Nam massa sem, pretium at lacus quis, aliquam facilisis odio. Maecenas risus purus, dapibus sed viverra a, efficitur eget leo. Integer quis magna id ante ornare euismod. Nunc aliquet arcu sit amet tincidunt feugiat. Ut et sapien ut leo blandit egestas a non arcu.</p><p>Sed finibus rhoncus nulla, eu molestie urna volutpat non. Etiam molestie faucibus nisi eget molestie. Vestibulum porta a leo a scelerisque. Mauris eget nisl sapien. Aliquam consectetur sed massa eget accumsan. Pellentesque eget arcu quam. Vivamus feugiat lacinia laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed quis quam vitae lorem rhoncus viverra vel et dolor. Sed pharetra cursus risus sit amet accumsan.</p>",
-      "tags": ["travel", "work"],
-      "date": "2017-03-05T10:25:43.511Z"
-    }
-    ],
-    title: "Messages"
-  },
-    computed: {
-    filteredEmails: function() {
-      let self = this;
+	$.getJSON("https://api.myjson.com/bins/dq3vy", function(data) {
+			$.each(data, function(i) {
+				if (i == "messages") {
+				$.each(this, function (key, value) {
+					$('.messages').append("<li class='message'><div class='inner_wrap'><h3 class='launch'>" + (value['subject']) + "</h3><h3 class='launch'>" + (value['sender']) + "</h3><div class='body'>" + (value['body']) + "</div><p>Date: " + (value['date']) + "</p><p class='tags'>" + (value['tags']) + "</p></div><input type='checkbox' class='checkbox'/><button class='remove_btn'> Delete </button><div class='modal hide'><h2>Message</h2><h3 class='launch'>" + (value['subject']) + "</h3><h4>Sender:  " + (value['sender']) + "</h4><p>" + (value['body']) + "</p></div></li>");
+				  });
+				}
+			});
+		});
 
-      let regex = /(<([^>]+)>)/ig;
+	$(document).on('click', '.checkbox', function(e){
+		if ($(this).parent().hasClass("checked")) {
+			$(this).parent().removeClass('star_meessage read');
+		}
+		$(this).parent().toggleClass("checked");
+	});
 
-      return this.messages.filter(function(message) {
-        let parts = self.emailSearch.trim().split(" ");
+	$(document).on('click', '#delete', function() {
+		  $('.checked').remove();
+	});
 
-        for(let part of parts) {
-
-          let searchRegex = new RegExp(part, 'i');
-
-          if(!(
-            searchRegex.test(message.subject) ||
-            searchRegex.test(message.sender) ||
-            searchRegex.test(message.body) ||
-            searchRegex.test(message.tags)
-          )) {
-            return false;
-          }
-        }
-
-        return true;
-
-      });
-    }
-  },
-  methods: {
-      deleteEvent: function(index) {
-        this.messages.splice(index, 1);
-      },
-      tagFilter: function(x) {
-           $('.message').hide();
-           $('.message').each(function() {
-               if($(this).children('.tags').text().indexOf(x) > -1) {
-                   $(this).show();
-               }
-           });
-     },
-      showAll: function() {
-          $('.message').show();
-      }
-  }
-});
-
-    $(":checkbox").on('click', function(){
-        if ($(this).parent().hasClass("checked")) {
-            $(this).parent().removeClass('star_meessage read');
-        }
-        $(this).parent().toggleClass("checked");
-    });
-
-    $('#delete').on('click', function() {
-          $('.checked').fadeOut();
-    });
-
-    $('#markRead').on('click', function() {
-          $('.checked').removeClass('star_message').addClass('read');
-    });
-    $('#starred').on('click', function() {
-          $('.checked').removeClass('read').addClass('star_meessage');
-    });
+	$(document).on('click', '#markRead', function() {
+		  $('.checked').removeClass('star_message').addClass('read');
+	});
+	$(document).on('click', '#starred', function() {
+		  $('.checked').removeClass('read').addClass('star_meessage');
+	});
 
 
-    //Toggle View
-    $('.view_btn').on('click', function() {
-        $('.view_btn').removeClass('active');
-        $(this).addClass('active');
-    });
-    $('#gridView').on('click', function() {
-       $('.message').addClass('grid');
-    });
-    $('#listView').on('click', function() {
-       $('.message').removeClass('grid');
-    });
+	//Toggle View
+	$('.view_btn').on('click', function() {
+		$('.view_btn').removeClass('active');
+		$(this).addClass('active');
+	});
+	$('#gridView').on('click', function() {
+	   $('.message').addClass('grid');
+	   $('.message .inner_wrap').addClass('grid_in');
+	});
+	$('#listView').on('click', function() {
+	   $('.message').removeClass('grid');
+	   $('.message .inner_wrap').removeClass('grid_in');
+	});
 
-    //Grid View content
-    $('.messages').on('click', '.grid', function() {
-        $('.modal_back').removeClass('hide');
-        $(this).children('.modal').removeClass('hide');
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-    });
-    $('.modal_back').on('click', function() {
-        $(this).addClass('hide');
-        $('.modal').addClass('hide');
-    });
+	//Grid View content
+	$(document).on('click', '.inner_wrap.grid_in', function() {
+		$(this).parent('.message').removeClass('animateShow animateHide');
+		$('.modal_back').removeClass('hide');
+		$(this).siblings('.modal').removeClass('hide');
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+	});
+	$('.modal_back').on('click', function() {
+		$(this).addClass('hide');
+		$('.modal').addClass('hide');
+	});
 
+
+	$(document).on('click', '.remove_btn', function() {
+		$(this).parent().remove();
+	});
+
+	$('.filter_btn_travel').on('click', function() {
+		   $('.message').removeClass('animateShow').addClass('animateHide').hide();
+		   $('.message').each(function() {
+			   if($(this).find('.tags').text().indexOf('travel') > -1) {
+			$(this).removeClass('animateHide').addClass('animateShow').show();
+			   }
+		  });
+	});
+	$('.filter_btn_work').on('click', function() {
+		   $('.message').removeClass('animateShow').addClass('animateHide').hide();
+		   $('.message').each(function() {
+			   if($(this).find('.tags').text().indexOf('work') > -1) {
+			$(this).removeClass('animateHide').addClass('animateShow').show();
+			   }
+		  });
+	});
+	$('.filter_btn_all').on('click', function() {
+		$('.message').addClass('animateShow').removeClass('animateHide').show();
+	});
+
+
+	//Sticky nav
+
+	var stickyTop = $('.tabs').offset().top;
+
+	$(window).on( 'scroll', function(){
+			if ($(window).scrollTop() >= stickyTop) {
+				$('.tabs').css({top: "15px"});
+			} else {
+				$('.tabs').css({top: "auto"});
+			}
+		});
+
+
+	//Search filter
+
+	$('#messageSearch').on('keyup', function() {
+		let value = $(this).val();
+		$( ".message").hide();
+		$( ".message:contains('"+value+"')" ).show();
+
+	});
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,24 @@ var webserver = require('gulp-webserver');
 
 gulp.task('sass', function(){
   return gulp.src('./app/scss/style.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('./app/css'));
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./app/css'));
 });
 
 gulp.task('webserver', function() {
   gulp.src('./app/')
-    .pipe(webserver({
-      livereload: true,
-      fallback: '/index.html',
-      //host: '0.0.0.0',
-      port: 9000,
-      directoryListing: false,
-      open: true
-    }));
+	.pipe(webserver({
+	  livereload: true,
+	  fallback: '/index.html',
+	  //host: '0.0.0.0',
+	  port: 9000,
+	  directoryListing: false,
+	  open: true
+	}));
 });
 
-gulp.task('default', [ 'sass', 'webserver' ]);
+gulp.task('sasswatch', function () {
+  gulp.watch('./app/scss/style.scss', ['sass']);
+});
+
+gulp.task('default', [ 'sasswatch', 'webserver' ]);
